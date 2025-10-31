484b2982e2d92e2c872065be8793b6832d8833ad
chethaka2003 <150814504+chethaka2003@users.noreply.github.com>
2025-10-31 09:13:46 +0530

    Init commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.add('show'); 
+}
+
+function hideSideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show'); 
+}
