24239a2b531bf92c1ab7a0fca6513e7e13c4a912
Prabash Vijayanga <prabashvijayanga@gmail.com>
2026-01-22 11:56:33 +0530

    Delete file

--- script.js
+++
@@ -1,9 +0,0 @@
-function showSideBar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.add('show'); 
-}
-
-function hideSideBar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.remove('show'); 
-}
