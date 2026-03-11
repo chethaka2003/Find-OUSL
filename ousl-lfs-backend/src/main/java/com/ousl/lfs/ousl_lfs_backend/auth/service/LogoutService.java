c438d5f3051858c472c3732d97a2f06cd4fb8e59
dinushkatharidu <asdinushkatharidu@gmail.com>
2026-03-11 12:04:36 +0530

    fix(backend): FR4 - LogoutService crashes on malformed token

--- ousl-lfs-backend/src/main/java/com/ousl/lfs/ousl_lfs_backend/auth/service/LogoutService.java
+++ ousl-lfs-backend/src/main/java/com/ousl/lfs/ousl_lfs_backend/auth/service/LogoutService.java
@@ -3,8 +3,10 @@ package com.ousl.lfs.ousl_lfs_backend.auth.service;
 import com.ousl.lfs.ousl_lfs_backend.auth.model.RevokedToken;
 import com.ousl.lfs.ousl_lfs_backend.auth.repo.RevokedTokenRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LogoutService {
@@ -13,12 +15,18 @@ public class LogoutService {
     private final RevokedTokenRepository revoked;
 
     public void logout(String rawToken) {
-        String jti = jwt.extractJti(rawToken);
 
-        RevokedToken rt = new RevokedToken();
-        rt.setJti(jti);
-        rt.setExpiresAt(jwt.extractExpiry(rawToken));
+        try{
 
-        revoked.save(rt);
+            String jti = jwt.extractJti(rawToken);
+            RevokedToken rt = new RevokedToken();
+            rt.setJti(jti);
+            rt.setExpiresAt(jwt.extractExpiry(rawToken));
+            revoked.save(rt);
+
+        } catch (Exception e) {
+            log.warn("Logout called with invalid/expired token: {}", e.getMessage());
+        }
+
     }
 }
